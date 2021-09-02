ec44878052b5bfb7bfd36f9a8b8016ae2cf28bb7
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-02 10:56:17 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,47 @@
 
 namespace DesposeDemo
 {
+    class ResourceManagement : IDisposable
+    {
+        private bool Disposed = false;
+        public ResourceManagement()
+        {
+            Console.WriteLine("Object is created");
+        }
+        ~ResourceManagement()
+        {
+            Console.WriteLine("Destructor called");
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public void Dispose(bool disposing)
+        {
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("Called From Dispose");
+                    //Clear all the managed resources here  
+                }
+                else
+                {
+                    //Clear all the unmanaged resources here  
+                }
+                Disposed = true;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ResourceManagement RM = new ResourceManagement();
+            RM.Dispose();
+            RM = null;
         }
     }
 }
