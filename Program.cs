d7a96c51c072ccda3a3adc859cd9cd9fc3c9721f
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-02 10:27:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesposeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
